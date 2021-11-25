9f004627d83bc9cfe2af03b5d3584ea80b653347
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-11-25 16:43:24 -0300

    initial commit

---
+++ src/main/java/com/lucasladeira/agendacontatos/AgendaContatosWebApplication.java
@@ -0,0 +1,13 @@
+package com.lucasladeira.agendacontatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaContatosWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaContatosWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasladeira/agendacontatos/AgendaContatosWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasladeira.agendacontatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaContatosWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
