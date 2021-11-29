8e8aaf38246776c9ffdc24b69c247d490fd8bd21
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-11-29 09:56:21 -0300

    ContatoService update implementation

--- src/main/java/com/lucasladeira/agendacontatos/service/ContatoService.java
+++ src/main/java/com/lucasladeira/agendacontatos/service/ContatoService.java
@@ -1,6 +1,10 @@
 package com.lucasladeira.agendacontatos.service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +22,33 @@ public class ContatoService {
 	public List<Contato> findAll(){
 		List<Contato> list = contatoRepository.findAll();
 		return list;
-	}	
+	}
+	
+	
+	public void save (Contato contato) {
+		contatoRepository.save(contato);
+	}
+	
+	public Contato update (Integer id, Contato oldContato) {
+		Optional<Contato> opt = contatoRepository.findById(oldContato.getId());
+		
+		if (opt.isEmpty()){
+			//erro
+		}
+		
+		Contato databaseContato = opt.get();
+		
+		updateData(databaseContato, oldContato);
+	
+		return contatoRepository.save(databaseContato);	
+	}
+	
+	//auxiliares
+	private void updateData(Contato databaseContato, Contato contato) {
+		databaseContato.setNome(contato.getNome());
+		databaseContato.setEmail(contato.getEmail());
+		databaseContato.setComentario(contato.getComentario());
+	}
+	
 	
 }
