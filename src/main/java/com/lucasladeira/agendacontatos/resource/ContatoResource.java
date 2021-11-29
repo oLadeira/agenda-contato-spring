e3fd5ca0c14b8db55fc95f41840ecd8c0f228b77
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-11-29 11:53:08 -0300

    Contato save implementation

--- src/main/java/com/lucasladeira/agendacontatos/resource/ContatoResource.java
+++ src/main/java/com/lucasladeira/agendacontatos/resource/ContatoResource.java
@@ -1,18 +1,22 @@
 package com.lucasladeira.agendacontatos.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucasladeira.agendacontatos.domain.Contato;
 import com.lucasladeira.agendacontatos.dto.ContatoDTO;
+import com.lucasladeira.agendacontatos.dto.NewContatoDTO;
 import com.lucasladeira.agendacontatos.service.ContatoService;
 
 @RestController
@@ -26,6 +30,15 @@ public class ContatoResource {
 	public ResponseEntity<List<Contato>> findAll(){
 		List<Contato> list = contatoService.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> save (@RequestBody NewContatoDTO newContatoDTO){
+		Contato contato = contatoService.fromDTO(newContatoDTO);
+		contatoService.save(contato);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(contato.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@PutMapping("/{id}")
--- src/main/java/com/lucasladeira/agendacontatos/service/ContatoService.java
+++ src/main/java/com/lucasladeira/agendacontatos/service/ContatoService.java
@@ -3,12 +3,18 @@ package com.lucasladeira.agendacontatos.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lucasladeira.agendacontatos.domain.Cidade;
 import com.lucasladeira.agendacontatos.domain.Contato;
+import com.lucasladeira.agendacontatos.domain.Endereco;
 import com.lucasladeira.agendacontatos.dto.ContatoDTO;
+import com.lucasladeira.agendacontatos.dto.NewContatoDTO;
 import com.lucasladeira.agendacontatos.repository.ContatoRepository;
+import com.lucasladeira.agendacontatos.repository.EnderecoRepository;
 
 @Service
 public class ContatoService {
@@ -16,15 +22,19 @@ public class ContatoService {
 	@Autowired
 	private ContatoRepository contatoRepository;
 	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public List<Contato> findAll(){
 		List<Contato> list = contatoRepository.findAll();
 		return list;
 	}
 	
-	
+	@Transactional
 	public void save (Contato contato) {
+		contato.setId(null);
 		contatoRepository.save(contato);
+		enderecoRepository.saveAll(contato.getEnderecos());
 	}
 	
 	public Contato update (Integer id, Contato oldContato) {
@@ -53,4 +63,22 @@ public class ContatoService {
 		return new Contato(contatoDTO.getId(), contatoDTO.getNome(), contatoDTO.getEmail(), contatoDTO.getComentario());
 	}
 	
+	public Contato fromDTO (NewContatoDTO newContatoDTO) {
+		Contato contato = new Contato(null, newContatoDTO.getNome(), newContatoDTO.getEmail(), newContatoDTO.getComentario());
+		Cidade cidade = new Cidade(newContatoDTO.getCidadeId(), null, null);
+		Endereco endereco = new Endereco(null, newContatoDTO.getRua(), newContatoDTO.getNumero(), newContatoDTO.getBairro(), newContatoDTO.getCep(), cidade, contato);
+		
+		contato.getEnderecos().add(endereco);
+		
+		contato.getTelefones().add(newContatoDTO.getTelefone());
+		
+		if (newContatoDTO.getTelefone2() != null) {
+			contato.getTelefones().add(newContatoDTO.getTelefone2());
+		}
+		
+		if (newContatoDTO.getTelefone3() != null) {
+			contato.getTelefones().add(newContatoDTO.getTelefone3());
+		}
+		return contato;
+	}
 }
